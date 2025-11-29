e92d8d5d71e5ae9bc120a929bfc801d3b608c356
ftmahrhmt <fatmahrohmahtika@gmail.com>
2025-11-29 11:48:54 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let contacts = [
+    {
+    nama: "Inggar Indraswari",
+    nomor: "0857-2231-8891",
+    email: "Inggarrr@gmail.com",
+    alamat: "Jakarta Selatan"
+  },
+  {
+    nama: "Annisa Naysillah Pramesta Putri",
+    nomor: "0813-5522-9011",
+    email: "icapuyu@gmail.com",
+    alamat: "Bandung, Jawa Barat"
+  },
+  {
+    nama: "Ananda Faturrahman",
+    nomor: "0822-9988-3321",
+    email: "nandaeman@gmail.com",
+    alamat: "Depok, Jawa Barat"
+  }
+];
+
+console.log(contacts)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./script.js"],
+  content: ["./index.html", "./js/script.js"],
   theme: {
     extend: {},
   },
